b2f894566d631ce5fb8cb4d465c25aed68758faf
illiyakorniyenko <illiya.korniyenko@student.hu.nl>
2019-01-31 17:01:52 +0100

    miniproject

--- startscherm.py
+++ startscherm.py
@@ -3,9 +3,14 @@ import requests
 import xmltodict
 
 
-def vertrektijdenWin(): #Deze functie is ons venster waar je de naam van het station in kan voeren.
+def vertrektijdenWin():
+    """Deze functie start het beginscherm.
+
+    In dit venster vindt je meerdere knoppen die verwijzen naar andere functies/vensters.
+    """
+
     newwin = Toplevel(root)
-    newwin.configure(bg='#ffc917') #achtergrondkleur
+    newwin.configure(bg='#ffc917')
     newwin.pack_propagate(0)
     newwin.geometry("800x600") #venstergrootte
 
@@ -48,7 +53,11 @@ def vertrektijdenWin(): #Deze functie is ons venster waar je de naam van het sta
     newwin.mainloop()
 
 
-def vertrektijden(stationnaam): #het venster met de vertrektijden
+def vertrektijden(stationnaam):
+    """Het venster met de vertrektijden.
+
+    Hier meerdere knoppen te vinden die doorverwijzen naar andere functies(vensters)
+    """
 
     newwin = Toplevel(root)
     newwin.configure(bg='#ffc917')
@@ -58,45 +67,40 @@ def vertrektijden(stationnaam): #het venster met de vertrektijden
     display.pack()
     topFrame = Frame(newwin)
     topFrame.pack()
-
     auth_details = ('illiya.korniyenko@student.hu.nl', 'Fn716EyB5F0QictFJSUdSoe9DKk2m0DNx54tdJTFmhb1gPNXaBhtsg') #inloggegevens voor de api.
     api_url = 'http://webservices.ns.nl/ns-api-avt?station='+ stationnaam.widget.get() #hier de link naar de vertrekkende tijden, het laatste deel van de link wordt aangevuld door wat de gebruiker in het vorig venster heeft ingevuld.
-
     response = requests.get(api_url, auth=auth_details)
     vertrekXML = xmltodict.parse(response.text)
 
 
-    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']: #uit het XML bestand pakken we alleen de tijden en de treinen, de rest hebben we niet nodig.
+    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+        """Uit het xml bestand pakken we de enige attributen die we hier nodig hebben
+        
+        
+        De attributen zijn: ActueleVertrektijden en VertrekkendeTrein
+        """
         eindbestemming = vertrek['EindBestemming']
-
         vertrektijd = vertrek['VertrekTijd']
         vertrektijd = vertrektijd[11:16] #alleen de 11 tot 16 letter van de string (de string begint met jaar > maand > dag, dat hebben we niet nodig, alleen uren en minuten.
-
         display = Label(newwin, bg="blue", fg="white", text='Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming) #wat er uiteindelijk wordt geprint op het scherm.
         display.pack(fill=BOTH, expand=1,)
 
     toolbar = Frame(newwin, bg='#003082')
-
     nederlandsbutton = Button(toolbar, text='Nederlands', bg='#003082', fg='Black')
     photo = PhotoImage(file='nederland.png')
     kpn = photo.subsample(6, 6)
     nederlandsbutton.config(image=kpn, compound=TOP)
     nederlandsbutton.pack(side=LEFT, padx=2, pady=2)
-
     engelsbutton = Button(toolbar, text='English', bg='#003082', fg='Black')
     picture = PhotoImage(file='engels.png')
     kpe = picture.subsample(21, 21)
     engelsbutton.config(image=kpe, compound=TOP)
     engelsbutton.pack(side=LEFT, padx=2, pady=2)
-
     mastervisa = PhotoImage(file='mastervisa.png')
-
     kpa = mastervisa.subsample(5, 5)
     mastervisalabel = Label(toolbar, image=kpa, compound=TOP)
     mastervisalabel.pack(side=RIGHT, padx=2, pady=2)
-
     toolbar.pack(side=BOTTOM, fill=X)
-
     newwin.mainloop()
 
 root = Tk()
